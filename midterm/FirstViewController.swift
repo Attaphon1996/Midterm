66a4b5e3df868e4e4d1866051889ea92af5a0045
อรรถพล วงษ์วิกิจการ <boatbright00@gmail.com>
2017-03-22 14:55:05 +0700

    Initial Commit

---
+++ midterm/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  midterm
+//
+//  Created by อรรถพล วงษ์วิกิจการ on 3/22/2560 BE.
+//  Copyright © 2560 อรรถพล วงษ์วิกิจการ. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
